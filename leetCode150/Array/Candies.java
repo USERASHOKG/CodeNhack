ff1806666c74316944dae545afd0f26694a22cb1
USERASHOKG <ashokashok.g895@gmail.com>
2024-06-23 14:51:00 +0530

    Distruuting candies based on rules

--- leetCode150/Array/Candies.java
+++ leetCode150/Array/Candies.java
@@ -1,1 +1,42 @@
+package CodeNhack.leetCode150.Array;
 
+import java.util.*;
+public class Candies{
+    public static int distrubute(int[] ratings){
+        int n = ratings.length;
+
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        Arrays.fill(left,1);
+        Arrays.fill(right,1);
+
+        int candies = 0;
+
+        //compare from 1 to left
+        for(int i=1; i<n; i++){
+            if (ratings[i] > ratings[i -1]) left[i] = left[i -1]+1;
+        }
+
+        //compare from n-2 to right neighbour
+        for(int i=n -2; i>=0; i--){
+            if(ratings[i] > ratings[i +1]) right[i] = right[i +1]+1;
+        }
+
+        //merge 2 arrays which is haviong max values
+        for(int i=0; i<n; i++){
+            candies += Math.max(right[i],left[i]);
+        }
+
+        return candies;
+        
+    }
+    public static void main(String[] args) {
+        int[] ratings = {1,0,2}; // {1,2,2} = 4
+
+        int s =distrubute(ratings);
+
+        System.out.println(s);
+        
+    }
+}
